9ef3f545ce2909db89524d4a5039cddb04fba7b9
atikure <mdatikure09@gmail.com>
2026-08-19 21:13:27 +0600

    counteries

--- lib/view/counteries.dart
+++ lib/view/counteries.dart
@@ -1,4 +1,4 @@
-import 'package:api_project/service/countries.dart';
+import 'package:api_project/view/custom_widget/text.dart';
 import 'package:flutter/material.dart';
 
 class Countries extends StatefulWidget {
@@ -11,28 +11,40 @@ class Countries extends StatefulWidget {
 class _CountriesState extends State<Countries> {
   List cl = [];
 
-  getData()async{
+  getData() async {
     var a = await CountriesService().getCountries();
     cl.clear();
     cl.addAll(a);
     setState(() {});
   }
+
   @override
   void initState() {
     getData();
     super.initState();
   }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(title: Text("Countries - ${cl.length}"), centerTitle: true,actions: [
-        IconButton(onPressed: (){
-          getData();
-        }, icon: Icon(Icons.refresh))      ],),
+      appBar: AppBar(
+        title: CustomText(text: "Countries - ${cl.length}"),
+        centerTitle: true,
+        actions: [
+          IconButton(
+            onPressed: () {
+              getData();
+            },
+            icon: Icon(Icons.refresh),
+          ),
+        ],
+      ),
       body: ListView.builder(
         itemCount: cl.length,
-        itemBuilder: (c, i) =>
-            ListTile(title: Text("${cl[i]["name"]}"), subtitle: Text("${cl[i]["admissionDateUnitedNations"]}")),
+        itemBuilder: (c, i) => ListTile(
+          title: Text("${cl[i]["name"]}"),
+          subtitle: Text("${cl[i]["admissionDateUnitedNations"]}"),
+        ),
       ),
     );
   }

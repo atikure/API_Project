f4e3b88e525beed70f3381c5b94a65569f9788be
atikure <mdatikure09@gmail.com>
2026-08-20 23:10:37 +0600

    product_list

--- lib/service/product_list.dart
+++ lib/service/product_list.dart
@@ -2,7 +2,7 @@ import 'dart:convert';
 
 import 'package:http/http.dart' as http;
 class ProductService{
-  Future <List> getProductList() async{ //Api
+  Future <List> getProductList() async{ //Fake Api
     var uri = Uri.parse("https://fakestoreapi.com/products");
     var r = await http.get(uri);
     return  jsonDecode(r.body);

ccefe55faa3a1f5fffca15c5cddf9da551cae0ff
atikure <mdatikure09@gmail.com>
2026-08-21 15:32:17 +0600

    book_list

---
+++ lib/service/book_list.dart
@@ -0,0 +1,11 @@
+
+import 'dart:convert';
+import 'package:http/http.dart' as http;
+
+class BookService{
+  Future <List> getBookList() async{
+    var uri = Uri.parse("https://fakeapi.extendsclass.com/books");
+    var r = await http.get(uri);
+    return  jsonDecode(r.body);
+  }
+}

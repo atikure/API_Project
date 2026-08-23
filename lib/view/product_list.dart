ce045b92ab21ecd1ebb2eac32dc8d8aa7b699049
atikure <mdatikure09@gmail.com>
2026-08-23 15:16:07 +0600

    product_list

--- lib/view/product_list.dart
+++ lib/view/product_list.dart
@@ -1,1 +1,12 @@
-// TODO Implement this library.
+import 'dart:convert';
+
+import 'package:http/http.dart' as http;
+class ProductService{
+  Future <List> getProductList() async{
+    var uri = Uri.parse("https://fakestoreapi.com/products");
+    var r = await http.get(uri);
+
+    return  jsonDecode(r.body);
+
+  }
+}

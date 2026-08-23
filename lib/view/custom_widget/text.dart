52e08f67d92c7fb14fffd25530af262919d10074
atikure <mdatikure09@gmail.com>
2026-08-23 15:22:33 +0600

    text

--- lib/view/custom_widget/text.dart
+++ lib/view/custom_widget/text.dart
@@ -22,6 +22,7 @@ class CustomText extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Text(
+      // CustomText
       text,
       maxLines: maxLine ?? 1,
       style: TextStyle(

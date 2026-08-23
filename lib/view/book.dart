06aa3ee2350ad30286d1613706ed837303cb771b
atikure <mdatikure09@gmail.com>
2026-08-23 14:59:12 +0600

    book

--- lib/view/book.dart
+++ lib/view/book.dart
@@ -100,9 +100,10 @@ class _BookScreenState extends State<BookScreen> {
                 Center(
                   child: ElevatedButton(
                     onPressed: () {},
-                    child: Text(
-                      "View More",
-                      style: TextStyle(color: Colors.black87, fontSize: 12),
+                    child: CustomText(
+                      text: "View More",
+                      color: Colors.black87,
+                      fSize: 15,
                     ),
                   ),
                 ),

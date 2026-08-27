82bc35c4f6ec387d5dd3170b622d466128111be9
atikure <mdatikure09@gmail.com>
2026-08-27 22:39:45 +0600

    text

--- lib/view/custom_widget/text.dart
+++ lib/view/custom_widget/text.dart
@@ -29,6 +29,7 @@ class CustomText extends StatelessWidget {
         color: color ?? Colors.black,
         fontSize: fSize ?? 12,
         fontWeight: fWeight ?? FontWeight.w400,
+        // fontWeight: fWeight ?? FontWeight.w400,
         overflow: tOverflow ?? TextOverflow.ellipsis,
         decoration: tDecoration ?? TextDecoration.none,
       ),

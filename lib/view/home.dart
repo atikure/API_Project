384ce42d604249f0b5f1ef1c95bccb526d7630b7
atikure <mdatikure09@gmail.com>
2026-08-29 21:44:11 +0600

    home

--- lib/view/home.dart
+++ lib/view/home.dart
@@ -33,7 +33,6 @@ class _ProductScreenState extends State<ProductScreen> {
       appBar: AppBar(
         backgroundColor: Colors.blueGrey,
         title: CustomText(text: "Product List", fSize: 25, color: Colors.black),
-        // title: CustomText(text: "Product List", fSize: 25, color: Colors.black),
         centerTitle: true,
       ),
       body: myProduct.length == 0

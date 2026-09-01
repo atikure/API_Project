8d128f63ed8592d00762bd4c49eea706975dd0e8
atikure <mdatikure09@gmail.com>
2026-09-01 21:04:53 +0600

    home

--- lib/view/home.dart
+++ lib/view/home.dart
@@ -50,7 +50,6 @@ class _ProductScreenState extends State<ProductScreen> {
                         decoration: BoxDecoration(
                           image: DecorationImage(
                             image: NetworkImage("${myProduct[index]["image"]}"),
-                            // image: NetworkImage("${myProduct[index]["image"]}"),
                           ),
                         ),
                       ),

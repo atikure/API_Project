185b8a074e8ade01a74d823f0f4621c3d7a8ea7e
atikure <mdatikure09@gmail.com>
2026-09-02 20:44:58 +0600

    product_list

--- lib/service/product_list.dart
+++ lib/service/product_list.dart
@@ -5,6 +5,7 @@ class ProductService{
   Future <List> getProductList() async{
     var uri = Uri.parse("https://fakestoreapi.com/products");
     var r = await http.get(uri);
+    // var r = await http.get(uri);
     return  jsonDecode(r.body);
   }
 }

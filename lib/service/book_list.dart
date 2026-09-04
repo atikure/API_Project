a1924222850fa0fbdf7d60b5b00400ab68e2feb5
atikure <mdatikure09@gmail.com>
2026-09-04 22:15:32 +0600

    book _list

--- lib/service/book_list.dart
+++ lib/service/book_list.dart
@@ -5,7 +5,6 @@ import 'package:http/http.dart' as http;
 class BookService{
   Future <List> getBookList() async{
     var uri = Uri.parse("https://fakeapi.extendsclass.com/books");
-    // var uri = Uri.parse("https://fakeapi.extendsclass.com/books");
     var r = await http.get(uri);
     return  jsonDecode(r.body);
   }

31348d20dc54a2f2dfe507bc2fedb218f1bc719f
atikure <mdatikure09@gmail.com>
2026-09-05 22:38:49 +0600

    counteries

--- lib/service/counteries.dart
+++ lib/service/counteries.dart
@@ -7,6 +7,7 @@ import 'dart:developer';
 class CountriesService{
   getCountries()async{
     var url = Uri.parse("https://fakeapi.extendsclass.com/countries");
+    // var url = Uri.parse("https://fakeapi.extendsclass.com/countries");
     var reponse = await http.get(url);
     if(reponse.statusCode == 200){
       return jsonDecode(reponse.body);
